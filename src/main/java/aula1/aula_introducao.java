f2d30e46d4bc856698307ec01f6faf3cc2737814
Laylateles <135770455+Laylateles@users.noreply.github.com>
2026-02-23 20:02:57 -0300

    aula-introdução

---
+++ src/main/java/aula1/aula_introducao.java
@@ -0,0 +1,10 @@
+package aula1;
+
+public class aula_introducao {
+    public static void main(String[] args){
+        String nome = "Inatel"; // string = palavra
+        System.out.println(nome); // para mostrar o nome no terminal
+        System.out.println(nome.length()); // mostra o tamanho da palavra
+        System.out.println(nome.toUpperCase()); // deixa em letra maiuscúla
+    }
+}
