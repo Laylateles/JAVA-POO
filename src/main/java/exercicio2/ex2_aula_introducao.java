e1ef6bb73326f5bb8fe30416865e6d1b4f075957
Laylateles <135770455+Laylateles@users.noreply.github.com>
2026-02-23 20:51:44 -0300

    segundo exercicio

---
+++ src/main/java/exercicio2/ex2_aula_introducao.java
@@ -0,0 +1,20 @@
+package exercicio2;
+
+public class ex2_aula_introducao {
+    public static void main(String[] args){
+        int ingressosInteiros = 8; // vale 30 cada
+        int ingressosMeia = 5; // vale 15 cada
+        double valorIngressoInteiro = 30.00;
+        double valorIngressoMeia = 15.00;
+        int quantidadeTotalIngressosVendidos = 13;
+        double ingressoInteiroVendido = ingressosInteiros * valorIngressoInteiro; // retorna o valor dos ingressos inteiros vendidos
+        double ingressoMeiaVendido = ingressosMeia * valorIngressoMeia;// valor do ingresso de meia-entrada
+        double valorTotalArrecadado = ingressoInteiroVendido + ingressoMeiaVendido; // retorna o valor total vendido na sessão
+        double mediaValorPagoIngressos = (ingressoInteiroVendido + ingressoMeiaVendido)/quantidadeTotalIngressosVendidos;
+
+        System.out.println("Total arrecadado com ingressos inteiros: " + ingressoInteiroVendido);
+        System.out.println("Total arrecadado com meia-entrada: " + ingressoMeiaVendido);
+        System.out.println("Total arrecado na sessão: " + valorTotalArrecadado);
+        System.out.println("Valor médio pago por ingresso: " + mediaValorPagoIngressos);
+    }
+}
