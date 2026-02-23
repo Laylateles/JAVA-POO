75f374367b1b8ac85a4d0901c4227a29a8af399e
Laylateles <135770455+Laylateles@users.noreply.github.com>
2026-02-23 20:30:27 -0300

    primeira-exercicio

---
+++ src/main/java/exercicio1/ex1_aula_introducao.java
@@ -0,0 +1,16 @@
+package exercicio1;
+
+public class ex1_aula_introducao {
+    public static void main(String[] args){
+        int horas = 3;
+        int primeiraHora = 10;
+        int segundaHora = 4;
+        int terceiraHora = 2;
+
+        int soma = primeiraHora + segundaHora + terceiraHora;
+        double media = soma / horas;
+
+        System.out.println("Total: " + soma);
+        System.out.println("Média: " + media);
+    }
+}
