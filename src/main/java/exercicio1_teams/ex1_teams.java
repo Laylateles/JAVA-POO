dafc46c1d14296f73c2c77cc82ebfd3a1d5d919b
Laylateles <135770455+Laylateles@users.noreply.github.com>
2026-03-05 17:19:40 -0300

    finalizando trabalho

--- src/main/java/exercicio1_teams/ex1_teams.java
+++ src/main/java/exercicio1_teams/ex1_teams.java
@@ -5,16 +5,34 @@ import java.util.Scanner;
 public class ex1_teams {
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
+
         System.out.println("Quantos ingressos de meia-entrada foram vendidos?");
         int meia = teclado.nextInt();
-        System.out.println("Qual o valor deles?");
-        String valor_meia = teclado.nextLine();
         teclado.nextLine();// limpa o buffer
+
+        System.out.println("Qual o valor de cada meia entrada:");
+        String valor_meia = teclado.nextLine();
+        valor_meia = valor_meia.replace(",",".");
+        double val_meia_conv = Double.valueOf(valor_meia);// faz a conversão de string para double, preciso fazer essa conversão
+
         System.out.println("Quantos ingressos de entrada inteira foram vendidos?");
         int inteiro = teclado.nextInt();
-        System.out.println("Qual o valor deles?");
-        teclado.nextLine();
+        teclado.nextLine();//limpa o buffer
+
+        System.out.println("Qual o valor de cada ingresso inteiro:");
         String valor_inteiro = teclado.nextLine();
-    // preciso tratar essas strings e converte-las para double!!
+        valor_inteiro = valor_inteiro.replace(",",".");
+        double val_int_conv = Double.valueOf(valor_inteiro);
+
+        double valor_total_meia = val_meia_conv * meia;
+        double valor_total_inteiro = val_int_conv * inteiro;
+        double valor_total_arrec = valor_total_inteiro + valor_total_meia;
+        double valor_medio_ingr = valor_total_arrec / (meia + inteiro);
+
+        System.out.printf("O valor total arrecadado com ingressos inteiros: %.2f\n" , valor_total_inteiro);
+        System.out.printf("O valor total arrecadado com ingressos meios: %.2f\n" , valor_total_meia);
+        System.out.printf("O valor total arrecadado na sessão: %.2f\n" , valor_total_arrec);
+        System.out.printf("O valor médio pago nos ingressos: %.2f\n" , valor_medio_ingr);
+
     }
 }
