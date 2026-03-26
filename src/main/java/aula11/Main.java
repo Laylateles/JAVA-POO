c93f8403ca606376e1e92305f848fa3ca72d5158
Laylateles <135770455+Laylateles@users.noreply.github.com>
2026-03-26 08:33:21 -0300

    array

--- src/main/java/aula11/Main.java
+++ src/main/java/aula11/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args){
         Salgado salgado = new Salgado();
         String nome = "Cantina do Inatel";
+        String salg1 = "Pizza";
+        String salg2 = "Enroladinho de salsicha";
+        String salg3 = "Empada";
 
+        //adicionar os métodos pra ver se ta funcionando
     }
 }
